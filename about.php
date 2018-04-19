0ae2c98c44c07b0372aaee9b28ad7581247065a7
Nisha Gupta <nishagupta@Nishas-iMac.local>
2018-04-19 18:28:48 +0530

    about us first section html

--- about.php
+++ about.php
@@ -6,146 +6,77 @@
   <meta name="keywords" content="About Giddh"/>
   <title>Giddh ~ About Us</title>
   <?php include 'head.php';?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+  
 
 </head>
-<body  ng-controller="homeController as vm" id="aboutPage" class="unresponsive lightTrans notAffix">
+<body  ng-controller="homeController as vm" id="aboutPage" class="about-section-body lightTrans notAffix">
   
   <ng-include src="'header.html'"></ng-include>
 
-  <div id="fullpage">
-    <div class="section">
-      <img class="img-responsive" src="/assets/images/new/about-banner.jpg" />
-      <div class="secBgWrap">
-        <div class="secBg secBg-zero">
-          <div class="sec-overlay">
-            
-            <div class="container">
-              <div class="row text-center">
-                <div class="col-sm-12">
-                  <h1 class="brand-heading arvo">It’s okay to make a mess<br>Our experiments have turned into amazing things</h1>
-                  <!-- <div id="abtCircle" class="arvo-b">
-                    ?
-                  </div> -->
-                </div>
-              </div>
+ <section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="about-head">
+                  <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                          <div class="area-flex">
+                                              <ul class="nav nav-tabs nav-tabs-about" role="tablist">
+                                                  <li role="presentation" class="active"><a href="#about" aria-controls="about" role="tab" data-toggle="tab">About Us</a></li>
+                                                  <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Our Values</a></li>
+                                                  <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Leadership Principles</a></li>
+                                                  <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">The Team</a></li>
+                                              </ul>
+
+                                        
+                                          </div>
+                            </div>
+                        </div>
+                  </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- first section -->
-
-    <div class="section" id="nrmTxtWrp">
-      <div class="container">
-        <div class="row fs24 abtTxt">
-          <div class="col-sm-12">
-            <p>Around 2 years ago, the idea of Giddh happened. The very first version was created that same time in core PHP, and it took many discussion sessions then. And finally after 1 year, we combined PHP with Laravel framework. There was a time when we completely scraped the earlier version in order to make data security our prime focus.</p>
-            <p>Today, as you can see, <a href="https://giddh.com" title="Giddh Accounting"> Giddh</a> runs on JAVA with spring and your data is secure than it was ever before. It took time, but as we look back today, the wait seems worth it.</p>
-            <p>It’s been long since people have been avoiding their business numbers, thinking them as maths. Plus, the UI of accounting software’s feels so heavy. Giddh’s UI combines the use of Node.js and Angular.js, to give you a smooth UI experience. <a href="https://giddh.com/gst.php" title="GST Accounting Software">GST Accounting</a> will not be maths anymore.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- second section -->
-
-
-    <div class="section">
-      <img class="img-responsive" src="/assets/images/new/webegan.jpg" />
-      <div class="secBgWrap">
-        <div class="secBg secBg-one">
-          <div class="sec-overlay">
-            <div class="container">
-              <div class="row text-center">
-                <div class="col-sm-12">
-                  <h3 class="bigger-text">We Began</h3>
-                  <p>Initially, we wanted to introduce everything in Giddh. But after giving a lot of thought, we decided to stick to very basic features. Today, we are focusing only on non-inventory accounting. We will move to other areas once we create disruption in the existing market.</p>
-                </div>
-              </div>
+            <div class="about-content">
+                  <div class="about-content-head">
+                      <div class="container">
+                            <h1>About Us</h1>
+                            <span>Pioneering the Automation Revolution in Accounting!</span>
+                      </div>
+                  </div>
+                    <div class="about-content-body">
+                      <div class="about">
+                                <div class="container">
+                                            <div class="tab-content">
+                                                    <div role="tabpanel" class="tab-pane active" id="about"><span class="company">Giddh</span> is a team with members who have accounting running in their blood, be it a member of development team or sales team. We are working relentlessly to push forward the automation revolution in bookkeeping industry with its tech solutions.<br><br>
+                                                     Since years Indian bookkeeping industry had fallen behind and failed to embrace the technological evolution.To change that once and forever, we have taken the initiative to keep adapting to the latest and greatest technologies to make accounting simple, fast and secure.<br><br>
+                                                      <a><i>As farsighted as a vulture:</i></a><br>
+                                                       As the name suggests, Giddh provides you with analytical tools that can help you to keep a sharp vision on your accounts so that you can invest your time on analysing them.<br><br>
+                                                        We believe in making accounting not only automated but simple like an elementary task by creating a product that can be used by everyone; from a simple web user who wants to simplify his accounting to a highly proficient business owners looking to scale up, above and ahead.</div>
+                                            </div>
+                                </div>
+                            </div>
+                     
+                    <div class="product">
+                      <div class="container">
+                        <div class="row">
+                              <div class="col-sm-12">
+                                    <h1>A product from Walkover</h1>
+                              </div>
+                          </div>
+                      </div>
+                    </div>
+                    </div>
             </div>
-          </div>
-        </div>
-      </div>
+        </div>              
     </div>
-    <!-- third section -->
-    
-    <div class="section">
-      <img class="img-responsive" src="/assets/images/new/theteam.jpg" />
-      <div class="secBgWrap">
-        <div class="secBg secBg-two">
-          <div class="sec-overlay">
-            <div class="container">
-              <div class="row text-center">
-                <div class="col-sm-12">
-                  <h3 class="bigger-text">The team</h3>
-                  <p>Small but mighty as we call them. There was a time when we were only a four people team. Now we are a pack of twelve crazy people who does not stop being curious. Discussions, paper flows, fights over features, we believe in everything insane.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- fourth section -->
+</section>
 
-    <div class="section">
-      <img class="img-responsive" src="/assets/images/new/culture.jpg" />
-      <div class="secBgWrap">
-        <div class="secBg secBg-three">
-          <div class="sec-overlay">
-            <div class="container">
-              <div class="row text-center">
-                <div class="col-sm-12">
-                  <h3 class="bigger-text">Culture</h3>
-                  <p>You’ll find a 2 AM work freak and a 6 AM early starter at our office. We are not chained by time, but by what the product needs. There is a guy who loves to curse the very core of the feature. There comes a time when it goes like developer vs QA, accountants vs developers, CEO vs his brain.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- five section -->
 
-    <div class="section">
-      <img class="img-responsive" src="/assets/images/new/youdecide.jpg" />
-      <div class="secBgWrap">
-        <div class="secBg secBg-four">
-          <div class="sec-overlay">
-            <div class="container">
-              <div class="row text-center">
-                <div class="col-sm-12">
-                  <h3 class="bigger-text">You decide</h3>
-                  <p>After much changes and thought, we are ready for you guys. We would love to hear about what more you could need to make your accounting life easier. Write to us, call us or leave us a feedback here. We would improve.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- six section -->
 
-    <div class="section" id="nrmTxtWrp1">
-      
-      <div class="container">
-        <div class="row text-center secBg-five">
-          <div id="abtMascot" class="visible-md-block visible-lg-block"></div>
-          <h2>Revolution is not made by technology, but by people!</h2>
-          <p class="arvo-b clearfix">Want to <a href="https://walkover.workable.com/" target="_Blank" class="btn btn-p-bdr btn-lg btn-bdr">work with us</a></p>
-        </div>
-      </div>
-       
-    </div>
-    <!-- seven section -->
-
-    <div class="section">
-      <ng-include src="'footer.html'"></ng-include>
-    </div>
-    <!-- eight section -->
-
-  </div>
   <!-- end of directive -->
   <?php include 'footScript.php';?>
+
   <script type="text/javascript">
     jQuery(document).ready(function($){
     'use strict';
@@ -166,5 +97,7 @@
       $(".navbar").removeClass('navbar-fixed-top')
     }
   </script>
+
+  
 </body>
 </html>
