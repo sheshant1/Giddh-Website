95a4e56aa80f0b6f5256dac3ec2b606377d132fd
sumitn543 <sumit@eseosolutions.com>
2018-05-16 18:30:23 +0530

    New Pages Updation Required
    Hi
    
    We have created some new pages, please upload the requested files

---
+++ retail-software.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html lang="en" ng-app="giddhWebsite">
+<head>
+  <meta name="description" content="Giddh GST accounting software simplifies the filing of GST return online. Enjoy automated filing with details like GST return form, tax invoice & GST online payments. Simple and easy to use, GST billing software and gst return software help to manage businesses smartly. No need to download! Try it for free today! "/>
+  <meta name="keywords" content="bookkeeping software, Accounting softwares, online accounting software, bookkeeping software for small business"/>
+  
+  <title>Online GST Return Filing Automation | Best GST Accounting Software India</title>
+
+  <?php include 'head.php';?>
+
+</head>
+
+<!-- {oh:geo.country != 'IN'} -->
+<body id="page-top" class="gst arvo" ng-controller="homeController as vm">
+  
+  <!-- <div ng-class="{'other-country': geo.country != 'IN'}" ng-if="geo.country != 'IN'">
+    <iframe src="global.html" style="height:100%;width:100%;padding: 0;margin:0"></iframe>
+  </div> -->
+
+
+
+  <ng-include src="'header.html'"></ng-include>
+
+  <!-- end navigation -->
+
+<!--Banner-->
+<div id="gstBanner" class="ylwBg gridBg">
+    <div class="container text-center">
+    	<img src="/assets/images/new/gst.png" class="img-responsive"/>
+        <h3>Goods and Service Tax (GST)</h3>
+    </div>
+</div>
+<!--//Banner-->
+
+<!--try giddh-->
+<div id="tryG">
+    <div class="container text-center">
+    	<p>Don't get confused</p>
+        <h3>File your GST RETURN automatically</h3><br>
+        
+        <a class="trBox" href="signup.php"><span>Try </span><img src="/assets/images/new/giddh-logo-white.png"/></a>
+        <p>Cloud Based Accounting Software to File GST return online</p>
+    </div>
+</div>
+<!--//try giddh-->
+
+<!--Filling process-->
+<div id="filling">
+    <div class="container text-center">
+    <div class="col-md-12 mrB3">
+	    	<h1>Retail Software</h1>
+            
+   
+           <p align="center"> Do you clothes, groceries, or electronics? Are you a owner of a general store? Whatever it is you may be running, Giddh helps you to take your account books beyond the usual cash register.</p>
+
+<p align="center">Managing a retail business is as difficult as it looks. With Giddh though, it's easy to be ahead of the curve and make better business decisions.</p>
+
+	    </div>
+    	
+        <div class="row">
+        
+        <h2>Tools That Can Help You Grow your Retail Business!
+</h2>
+        
+
+
+<p>Create new sales orders, or add a general journal entry to keep track of your sales. Calculate purchases made over the time and be on top of your accounting.
+</p>
+
+<p>
+Track inventory status, items to be added in the inventory and analyse your opening and closing stocks at the end of the month.
+</p>
+
+<p>Always stay connected to your financial records even when you are on the go thanks to our app.</p>
+
+        
+        </div>
+        
+    </div>
+</div>
+<!--//Filling process-->
+
+<!--question-->
+<div id="ques">
+    <div class="container text-center">
+        <h3>Have question on GST Accounting? Have free discussion <a href="#contact">Contact Us</a></h3>
+    </div>
+</div>
+<!--//question-->
+
+<!--Advantages-->
+<div id="adv">
+    <div class="container text-center">
+    	<h3>Advantages of filing GST Return with us</h3>
+    	<div class="col-md-3 col-xs-6">
+        	<div class="ic-52 circ"></div>
+            <br>100%<br> accurate
+        </div>
+        <div class="col-md-3 col-xs-6">
+        	<div class="ic-52 tick"></div>
+            <br>Easy, Secure<br>and Fast
+        </div>
+        <div class="col-md-3 col-xs-6">
+        	<div class="ic-52 ldm"></div>
+            <br>Status<br> Tracking
+        </div>
+        <div class="col-md-3 col-xs-6">
+        	<div class="ic-52 lock"></div>
+            <br>Full<br> confidential
+        </div>
+    </div>
+</div>
+
+<!--//Advantages-->
+
+<!-- Collaborators -->
+<div class="cmnPd80 ylwBg">
+    <div class="container text-center">
+        <h3 class="mrB5 mrT">GST Return Collaborators</h3>
+        <div class="pdT4 clearfix">
+            <div class="col-md-4 col-xs-12 collaborator">
+                <div>
+                    <img src="assets/images/collaborator1.png" alt="jio gst" />
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 collaborator bdrL bdrR">
+                <div>
+                    <img src="assets/images/collaborator2.png" alt="cleartax" />
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 collaborator">
+                <div>
+                    <img src="assets/images/collaborator3.png" alt="tax raahi" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //Collaborators -->
+
+
+  <!-- include footer -->
+  <?php include 'footScript.php';?>
+  <?php include 'footer.php';?>  
+</body>
+</html>
---
+++ tourism-software.php
@@ -0,0 +1,144 @@
+<!DOCTYPE HTML>
+<html lang="en" ng-app="giddhWebsite">
+<head>
+  <meta name="description" content="Giddh GST accounting software simplifies the filing of GST return online. Enjoy automated filing with details like GST return form, tax invoice & GST online payments. Simple and easy to use, GST billing software and gst return software help to manage businesses smartly. No need to download! Try it for free today! "/>
+  <meta name="keywords" content="bookkeeping software, Accounting softwares, online accounting software, bookkeeping software for small business"/>
+  
+  <title>Online GST Return Filing Automation | Best GST Accounting Software India</title>
+
+  <?php include 'head.php';?>
+
+</head>
+
+<!-- {oh:geo.country != 'IN'} -->
+<body id="page-top" class="gst arvo" ng-controller="homeController as vm">
+  
+  <!-- <div ng-class="{'other-country': geo.country != 'IN'}" ng-if="geo.country != 'IN'">
+    <iframe src="global.html" style="height:100%;width:100%;padding: 0;margin:0"></iframe>
+  </div> -->
+
+
+
+  <ng-include src="'header.html'"></ng-include>
+
+  <!-- end navigation -->
+
+<!--Banner-->
+<div id="gstBanner" class="ylwBg gridBg">
+    <div class="container text-center">
+    	<img src="/assets/images/new/gst.png" class="img-responsive"/>
+        <h3>Goods and Service Tax (GST)</h3>
+    </div>
+</div>
+<!--//Banner-->
+
+<!--try giddh-->
+<div id="tryG">
+    <div class="container text-center">
+    	<p>Don't get confused</p>
+        <h3>File your GST RETURN automatically</h3><br>
+        
+        <a class="trBox" href="signup.php"><span>Try </span><img src="/assets/images/new/giddh-logo-white.png"/></a>
+        <p>Cloud Based Accounting Software to File GST return online</p>
+    </div>
+</div>
+<!--//try giddh-->
+
+<!--Filling process-->
+<div id="filling">
+    <div class="container text-center">
+    <div class="col-md-12 mrB3">
+	    	<h1>Tourism Business</h1>
+            
+            <p align="center">Managing a business in the travel sector can be challenging. With Giddh, create professional invoices, keep track of your transactions & easily automate your operations. Maintain reliable accounting whether you own a hotel or travel & tour agency.</p>
+	    </div>
+    	
+        <div class="row">
+        
+        <h2>Ways Your Tourism Business Can Fly High with Giddh!</h2>
+        
+
+<h3>Power of Data, Anywhere-Anytime-Anyone!</h3>
+
+<p>Giddh's cloud based platform allows any third party to quickly identify files that require attention during accounting periods without wasting time. Saving time means saving more money for your tourism business!</p>
+
+<h3>Analyse and Evaluate at One Glance!</h3>
+
+<p>See how things are going with a glance at the comprehensive and visually stunning charts on the Giddh dashboard. Curb your unnecessary expenses and save more to earn more!</p>
+
+<h3>On the Go? Giddh's Got you Covered!</h3>
+
+<p>For the business owners who are always on the move, Giddh's mobile app is an excellent medium to keep track of transactions to help you stay ahead of the curve! </p>
+
+
+        
+        </div>
+        
+    </div>
+</div>
+<!--//Filling process-->
+
+<!--question-->
+<div id="ques">
+    <div class="container text-center">
+        <h3>Have question on GST Accounting? Have free discussion <a href="#contact">Contact Us</a></h3>
+    </div>
+</div>
+<!--//question-->
+
+<!--Advantages-->
+<div id="adv">
+    <div class="container text-center">
+    	<h3>Advantages of filing GST Return with us</h3>
+    	<div class="col-md-3 col-xs-6">
+        	<div class="ic-52 circ"></div>
+            <br>100%<br> accurate
+        </div>
+        <div class="col-md-3 col-xs-6">
+        	<div class="ic-52 tick"></div>
+            <br>Easy, Secure<br>and Fast
+        </div>
+        <div class="col-md-3 col-xs-6">
+        	<div class="ic-52 ldm"></div>
+            <br>Status<br> Tracking
+        </div>
+        <div class="col-md-3 col-xs-6">
+        	<div class="ic-52 lock"></div>
+            <br>Full<br> confidential
+        </div>
+    </div>
+</div>
+
+<!--//Advantages-->
+
+<!-- Collaborators -->
+<div class="cmnPd80 ylwBg">
+    <div class="container text-center">
+        <h3 class="mrB5 mrT">GST Return Collaborators</h3>
+        <div class="pdT4 clearfix">
+            <div class="col-md-4 col-xs-12 collaborator">
+                <div>
+                    <img src="assets/images/collaborator1.png" alt="jio gst" />
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 collaborator bdrL bdrR">
+                <div>
+                    <img src="assets/images/collaborator2.png" alt="cleartax" />
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 collaborator">
+                <div>
+                    <img src="assets/images/collaborator3.png" alt="tax raahi" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //Collaborators -->
+
+
+  <!-- include footer -->
+  <?php include 'footScript.php';?>
+  <?php include 'footer.php';?>  
+</body>
+</html>
