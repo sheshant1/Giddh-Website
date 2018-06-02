be25af96a2e2919df9211b2a2d676666e33533b7
sumitn543 <sumit@eseosolutions.com>
2018-06-02 12:09:13 +0530

    Retail & Inventory 2 Page

--- Inventory-Management-software.php
+++ Inventory-Management-software.php
@@ -26,8 +26,8 @@
 <!--Banner-->
 <div id="gstBanner" class="ylwBg gridBg">
     <div class="container text-center">
-    	<img src="/assets/images/new/gst.png" class="img-responsive"/>
-        <h3>Goods and Service Tax (GST)</h3>
+    	<img src="/assets/images/new/Inventory-Management-software.png" class="img-responsive"/>
+        <h1>Inventory Management Software</h1>
     </div>
 </div>
 <!--//Banner-->
@@ -35,36 +35,30 @@
 <!--try giddh-->
 <div id="tryG">
     <div class="container text-center">
-    	<p>Don't get confused</p>
-        <h3>File your GST RETURN automatically</h3><br>
+        <h3>Cloud Based Accounting Software for Businesses of All Sizes</h3>
         
-        <a class="trBox" href="signup.php"><span>Try </span><img src="/assets/images/new/giddh-logo-white.png"/></a>
-        <p>Cloud Based Accounting Software to File GST return online</p>
+        <p>
+        Modern Inventory Management Software for the Modern Entrepreneur 
+                                   No stress, No hassles, No hidden costs.
+        </p>
+        
+          
+        <p>
+        Managing inventory is a priority for any growing business. Giddh's inventory management feature helps you control and track your stock movements accurately so you can get back to doing the thing you are best at: Grow and Earn!
+
+        </p>
+
     </div>
 </div>
 <!--//try giddh-->
 
 <!--Filling process-->
 <div id="filling">
-    <div class="container text-center">
-    <div class="col-md-12 mrB3">
-	    	<h1> Cloud Based Accounting Software for Businesses of All Sizes</h1>
-            
-            <p align="center">Modern Inventory Management Software for the Modern Entrepreneur 
-                                   No stress, No hassles, No hidden costs.
-</p>
-	    </div>
+    <div class="container">
+  
     	
-        <div class="row">
-        
-        <p align="justify">
-        Managing inventory is a priority for any growing business. Giddh's inventory management feature helps you control and track your stock movements accurately so you can get back to doing the thing you are best at: Grow and Earn!
-
-        </p>
-        
-     
-        
-
+        <div class="col-lg-6">
+      
 <h3>Complete Inventory Management </h3>
 
 <p>Easily add services rendered or product manufactured. With Giddh's inventory management features, keeping track of your inventory and stocks is as easy as counting 1,2,3.</p>
@@ -72,6 +66,41 @@
 <h3>Full Support for Measurement Units</h3>
 
 <p>When measurement units are concerned, Giddh comes with limitless combination of units. From hours to boxes to kilograms, use whatever measure you need. </p>
+
+
+
+        </div>
+        
+        
+        <div class="col-lg-6">
+        
+        <img src="/assets/images/new/invetn-xyz.png" class="img-responsive"/>
+        
+        </div>
+        
+        
+        
+        
+    </div>
+</div>
+<!--//Filling process-->
+
+
+<!--Filling process-->
+<div id="ques">
+    <div class="container">
+  
+  
+     
+        <div class="col-lg-6">
+        
+        <img src="/assets/images/new/GST_multi_cru.png" class="img-responsive"/>
+        
+        </div>
+        
+        
+    	
+        <div class="col-lg-6">
 
 <h3>GST Ready</h3>
 
@@ -81,79 +110,57 @@
 
 <p>Giddh's multi-currency functionality automatically converts amounts to your mentioned base currency to maintain consistency across all stock actions.</p>
 
-<h3>Create Detailed Professional Invoices</h3>
-
-<p>Create fully detailed invoices with Giddh's customizable invoicing options. No other accounting software puts so much emphasis on the design of documents. </p>
 
 
-
-
-
-
-        
         </div>
+
         
     </div>
 </div>
 <!--//Filling process-->
 
+
+
+<!--Filling process-->
+<div id="filling">
+    <div class="container">
+  
+    	
+        <div class="col-lg-6">
+
+<h3>Create Detailed Professional Invoices</h3>
+
+<p>Create fully detailed invoices with Giddh's customizable invoicing options. No other accounting software puts so much emphasis on the design of documents. </p>
+
+
+        </div>
+        
+        
+        <div class="col-lg-6">
+        
+        <img src="/assets/images/new/invoice-xyz.png" class="img-responsive"/>
+        
+        </div>
+        
+        
+        
+        
+    </div>
+</div>
+<!--//Filling process-->
+
+
+
+
 <!--question-->
 <div id="ques">
     <div class="container text-center">
-        <h3>Have question on GST Accounting? Have free discussion <a href="#contact">Contact Us</a></h3>
+        <h3>Have question on Inventory Management? Have free discussion <a href="#contact">Contact Us</a></h3>
     </div>
 </div>
 <!--//question-->
 
-<!--Advantages-->
-<div id="adv">
-    <div class="container text-center">
-    	<h3>Advantages of filing GST Return with us</h3>
-    	<div class="col-md-3 col-xs-6">
-        	<div class="ic-52 circ"></div>
-            <br>100%<br> accurate
-        </div>
-        <div class="col-md-3 col-xs-6">
-        	<div class="ic-52 tick"></div>
-            <br>Easy, Secure<br>and Fast
-        </div>
-        <div class="col-md-3 col-xs-6">
-        	<div class="ic-52 ldm"></div>
-            <br>Status<br> Tracking
-        </div>
-        <div class="col-md-3 col-xs-6">
-        	<div class="ic-52 lock"></div>
-            <br>Full<br> confidential
-        </div>
-    </div>
-</div>
 
-<!--//Advantages-->
-
-<!-- Collaborators -->
-<div class="cmnPd80 ylwBg">
-    <div class="container text-center">
-        <h3 class="mrB5 mrT">GST Return Collaborators</h3>
-        <div class="pdT4 clearfix">
-            <div class="col-md-4 col-xs-12 collaborator">
-                <div>
-                    <img src="assets/images/collaborator1.png" alt="jio gst" />
-                </div>
-            </div>
-            <div class="col-md-4 col-xs-12 collaborator bdrL bdrR">
-                <div>
-                    <img src="assets/images/collaborator2.png" alt="cleartax" />
-                </div>
-            </div>
-            <div class="col-md-4 col-xs-12 collaborator">
-                <div>
-                    <img src="assets/images/collaborator3.png" alt="tax raahi" />
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- //Collaborators -->
 
 
   <!-- include footer -->
